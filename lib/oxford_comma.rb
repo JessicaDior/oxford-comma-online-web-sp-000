8d8fa56f1aa56133e5c21778dda7eb9bc3de294e
Jc Dior <jessicaclairdior@gmail.com>
2020-08-26 23:51:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif
       array.length > 2
       array[-1] = "and #{array[-1]}"
-      array.join(" , ")
+      array.join(", ")
     else
       array.join(" , ")
   end
